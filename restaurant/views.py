f0a0c0021dde983e59b0bade0e14fe432b224cfd
Musipe <mulwanasimon7@gmail.com>
2024-05-24 15:14:13 +0300

    Booking api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Menu
-from .serializers import MenuSerializer, UserSerializer
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework import viewsets
+from .serializers import MenuSerializer, UserSerializer, BookingSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
@@ -11,3 +14,13 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
