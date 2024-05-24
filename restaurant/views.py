7a5b500854f0ce54f002f196125bf567acfba033
Musipe <mulwanasimon7@gmail.com>
2024-05-24 13:25:48 +0300

    Api initials

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Menu
+from .serializers import MenuSerializer, UserSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
